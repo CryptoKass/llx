dd0e5f8a7efba1d4a791e023092f70de76633b98
CryptoKass <kasscrypto@gmail.com>
2025-01-30 21:51:55 +0000

    add helpers for token details

---
+++ src/token.ts
@@ -0,0 +1,100 @@
+import type { Abi, Address, PublicClient } from "viem";
+import { readContract } from "viem/actions";
+
+const TokenABI: Abi = [
+  {
+    name: "name",
+    type: "function",
+    stateMutability: "view",
+    inputs: [],
+    outputs: [{ type: "string" }],
+  },
+  {
+    name: "symbol",
+    type: "function",
+    stateMutability: "view",
+    inputs: [],
+    outputs: [{ type: "string" }],
+  },
+  {
+    name: "decimals",
+    type: "function",
+    stateMutability: "view",
+    inputs: [],
+    outputs: [{ type: "uint8" }],
+  },
+  {
+    name: "totalSupply",
+    type: "function",
+    stateMutability: "view",
+    inputs: [],
+    outputs: [{ type: "uint256" }],
+  },
+];
+
+export interface TokenDetails {
+  name: string;
+  symbol: string;
+  decimals: number;
+  totalSupply: bigint;
+}
+
+export const getTokenName = async (
+  publicClient: PublicClient,
+  address: Address
+) => {
+  const name = await publicClient.readContract({
+    address,
+    abi: TokenABI,
+    functionName: "name",
+  });
+
+  return name as string;
+};
+
+export const getTokenSymbol = async (
+  publicClient: PublicClient,
+  address: Address
+) => {
+  const symbol = await publicClient.readContract({
+    address,
+    abi: TokenABI,
+    functionName: "symbol",
+  });
+};
+
+export const getTokenDecimals = async (
+  publicClient: PublicClient,
+  address: Address
+) => {
+  const decimals = await publicClient.readContract({
+    address,
+    abi: TokenABI,
+    functionName: "decimals",
+  });
+};
+
+export const getTokenTotalSupply = async (
+  publicClient: PublicClient,
+  address: Address
+) => {
+  const totalSupply = await publicClient.readContract({
+    address,
+    abi: TokenABI,
+    functionName: "totalSupply",
+  });
+};
+
+export const getTokenDetails = async (
+  publicClient: PublicClient,
+  address: Address
+) => {
+  const [name, symbol, decimals, totalSupply] = await Promise.all([
+    getTokenName(publicClient, address),
+    getTokenSymbol(publicClient, address),
+    getTokenDecimals(publicClient, address),
+    getTokenTotalSupply(publicClient, address),
+  ]);
+
+  return { name, symbol, decimals, totalSupply };
+};
