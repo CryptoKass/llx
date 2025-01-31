97a22658fdcd773e9e31a8a585398efb3030ae09
CryptoKass <kasscrypto@gmail.com>
2025-01-31 00:42:17 +0000

    set elektric exports

---
+++ src/elektric/index.ts
@@ -0,0 +1,5 @@
+export { quoteExactInput } from "./quote.js";
+export { swapExactInput } from "./swap.js";
+
+export type { QuoteExactInputSingleParams, QuoteResult } from "./quote.js";
+export type { SwapExactInputParams, SwapExactInputResult } from "./swap.js";
