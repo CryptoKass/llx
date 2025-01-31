94f9f9dba6023a2a34f6eea0b6c1426902d45e26
CryptoKass <kasscrypto@gmail.com>
2025-01-31 00:44:06 +0000

    add index exports for various modules

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+export { search, getContractInfo } from "./explorer/index.js";
+export { quoteExactInput, swapExactInput } from "./elektric/index.js";
+export { resolveEnsDomain, resolveLLDomain } from "./ens.js";
+export {
+  getTokenDetails,
+  getTokenDecimals,
+  getTokenName,
+  getTokenSymbol,
+  getTokenTotalSupply,
+} from "./token.js";
